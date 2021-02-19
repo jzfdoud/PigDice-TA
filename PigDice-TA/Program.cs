6a957e6c7e62ea1960360d3ce0d376ca03cbb614
Jada <jada.fagan@gmail.com>
2021-02-19 15:13:52 -0500

    Add project files.

---
+++ PigDice-TA/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PigDice_TA
+{
+    class Program
+    {
+
+
+        static int RollDie()
+        {
+            var rnd = new Random();
+            return rnd.Next(1,7);
+        }
+
+        static int PlayPigDice()
+        {
+            var score = 0;
+            var roll = RollDie();
+            while (roll != 1)
+            {
+                score += roll;
+                roll = RollDie();
+            }
+            return score;
+        }
+
+
+        static void Main(string[] args)
+        {
+            var score = PlayPigDice();
+            Console.WriteLine($"Score: {score}");
+        }
+    }
+}
